d01dad7fcf3f3433af4648be88f90f1a0796250e
muralikaturi <murali.katuri@hmcts.net>
2019-04-08 12:57:09 +0100

    Added dummy test to generate a test coverage file

--- BookingQueueSubscriber/BookingQueueSubscriber.UnitTests/EventHandlerTests.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.UnitTests/EventHandlerTests.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace BookingQueueSubscriber.UnitTests
 {
     public class EventHandlerTests
     {
+        [Test]
+        public void should_pass_dummytest()
+        {
+            true.Should().BeTrue();
+        }
     }
 }
