c8fd8ca3789f84909048b186ed1b42e3adb19474
Robin Weller <robin@saucecode.co>
2021-02-09 11:33:58 +0000

    VIH-7058 Local Test Environment Switching (#52)
    * Local Test Environment Switching
    
    * feature/VIH-7058 : Added Test3 env
    
    Co-authored-by: Nicholas Poxon <NicholasPoxon1@hmcts.net>

--- BookingQueueSubscriber/BookingQueueSubscriber.AcceptanceTests/Hooks/Setup.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.AcceptanceTests/Hooks/Setup.cs
@@ -1,4 +1,6 @@
-﻿using AcceptanceTests.Common.Configuration;
+﻿using System;
+using AcceptanceTests.Common.Configuration;
+using AcceptanceTests.Common.Exceptions;
 using BookingQueueSubscriber.AcceptanceTests.Configuration;
 using BookingQueueSubscriber.Common.Configuration;
 using BookingQueueSubscriber.Common.Security;
@@ -16,14 +18,9 @@ namespace BookingQueueSubscriber.AcceptanceTests.Hooks
         public Setup()
         {
             _context = new TestContext();
-            _configRoot = ConfigurationManager.BuildConfig("F6705640-D918-4180-B98A-BAB7ADAA4817", GetTargetEnvironment());
+            _configRoot = ConfigurationManager.BuildConfig("F6705640-D918-4180-B98A-BAB7ADAA4817", "0A28103E-8F77-4BCB-85A6-2D3279598623");
             _context.Config = new Config();
             _context.Tokens = new Tokens();
-        }
-
-        private static string GetTargetEnvironment()
-        {
-            return NUnit.Framework.TestContext.Parameters["TargetEnvironment"] ?? "";
         }
 
         public TestContext RegisterSecrets()
@@ -54,10 +51,14 @@ namespace BookingQueueSubscriber.AcceptanceTests.Hooks
 
         private IOptions<ServicesConfiguration> RegisterServices()
         {
-            var services = Options.Create(_configRoot.GetSection("VhServices").Get<ServicesConfiguration>());
-            _context.Config.Services = services.Value;
-            _context.Config.Services.BookingsApiUrl.Should().NotBeNullOrEmpty("BookingsApiUrl is set");
-            _context.Config.Services.VideoApiUrl.Should().NotBeNullOrEmpty("VideoApiUrl is set");
+            var services = GetTargetTestEnvironment() == string.Empty ? 
+                Options.Create(_configRoot.GetSection("VhServices").Get<ServicesConfiguration>()) 
+                : Options.Create(_configRoot.GetSection($"Testing.{GetTargetTestEnvironment()}.VhServices").Get<ServicesConfiguration>());
+            if (services == null && GetTargetTestEnvironment() != string.Empty)
+                throw new TestSecretsFileMissingException(GetTargetTestEnvironment());
+            _context.Config.Services = services?.Value;
+            _context.Config.Services?.BookingsApiUrl.Should().NotBeNullOrEmpty("BookingsApiUrl is set");
+            _context.Config.Services?.VideoApiUrl.Should().NotBeNullOrEmpty("VideoApiUrl is set");
             return services;
         }
 
@@ -65,6 +66,11 @@ namespace BookingQueueSubscriber.AcceptanceTests.Hooks
         {
             _context.Config.UsernameStem = _configRoot.GetValue<string>("UsernameStem");
             _context.Config.UsernameStem.Should().NotBeNullOrEmpty("UsernameStem is set");
+        }
+
+        private static string GetTargetTestEnvironment()
+        {
+            return NUnit.Framework.TestContext.Parameters["TargetTestEnvironment"] ?? string.Empty;
         }
 
         private void GenerateBearerTokens(AzureAdConfiguration azureOptions, IOptions<ServicesConfiguration> services)
