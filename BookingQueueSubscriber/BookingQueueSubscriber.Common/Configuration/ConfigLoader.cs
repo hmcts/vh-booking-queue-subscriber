fe33e71c5825a38271177930e26e7262be38a13e
Chris Tumolo <2236507+tremolo64@users.noreply.github.com>
2021-02-10 09:12:25 +0000

    Add SDS support (#48)
    Co-authored-by: Chris Tumolo <chris.tumolo@HMCTS.net>

--- BookingQueueSubscriber/BookingQueueSubscriber.Common/Configuration/ConfigLoader.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.Common/Configuration/ConfigLoader.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.Configuration;
+using VH.Core.Configuration;
 
 namespace BookingQueueSubscriber.Common.Configuration
 {
@@ -8,10 +9,16 @@ namespace BookingQueueSubscriber.Common.Configuration
 
         public ConfigLoader()
         {
+            const string vhInfraCore = "/mnt/secrets/vh-infra-core";
+            const string vhBookingQueueSubscriber = "/mnt/secrets/vh-booking-queue-subscriber";
+
             var configRootBuilder = new ConfigurationBuilder()
-                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables();
+                .AddJsonFile("local.settings.json", true, true)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddEnvironmentVariables()
+                .AddAksKeyVaultSecretProvider(vhInfraCore)
+                .AddAksKeyVaultSecretProvider(vhBookingQueueSubscriber);
+
             Configuration = configRootBuilder.Build();
         }
     }
