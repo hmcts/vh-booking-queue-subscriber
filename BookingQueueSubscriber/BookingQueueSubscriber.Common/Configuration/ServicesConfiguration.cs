1e95aa4a114ed272d8236fa762999286dd295d6e
shaed-parkar <41630528+shaed-parkar@users.noreply.github.com>
2021-02-11 09:51:40 +0000

    hardcode authority (#55)

--- BookingQueueSubscriber/BookingQueueSubscriber.Common/Configuration/ServicesConfiguration.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.Common/Configuration/ServicesConfiguration.cs
@@ -3,8 +3,8 @@ namespace BookingQueueSubscriber.Common.Configuration
     public class ServicesConfiguration
     {
         public string BookingsApiUrl { get; set; }
-        public string VideoApiResourceId { get; set; } = "https://localhost";
-        public string VideoApiUrl { get; set; } = "https://localhost";
+        public string VideoApiResourceId { get; set; }
+        public string VideoApiUrl { get; set; }
         public bool EnableVideoApiStub { get; set; }
     }
 }
