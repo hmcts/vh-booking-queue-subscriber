bafbce8425e866d80fdd92c40d1edd1ffb121e4e
Saravanan Radhakrishnan <shravan_krish@yahoo.com>
2021-02-23 09:42:33 +0000

    VIH-6958 updated acceptance tests with linkedparticipants (#63)
    * Updated acceptance tests
    
    * Updated acceptance configuration
    
    * Updated resourceids on pipeline
    
    * Updated setup to resolve build issue
    
    * Updated participant to have unique name to resolve issue
    
    Co-authored-by: Saravanan Radha Krishnan <Saravanan.Krishnan@HMCTS.NET>

--- BookingQueueSubscriber/BookingQueueSubscriber.Common/Configuration/ServicesConfiguration.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.Common/Configuration/ServicesConfiguration.cs
@@ -3,6 +3,7 @@ namespace BookingQueueSubscriber.Common.Configuration
     public class ServicesConfiguration
     {
         public string BookingsApiUrl { get; set; }
+        public string BookingsApiResourceId { get; set; }
         public string VideoApiResourceId { get; set; }
         public string VideoApiUrl { get; set; }
         public bool EnableVideoApiStub { get; set; }
