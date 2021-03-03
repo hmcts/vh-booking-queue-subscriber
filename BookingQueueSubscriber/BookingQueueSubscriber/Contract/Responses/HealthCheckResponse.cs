3e9a872b1ce3561ab670f160fec6a51e2c3f4dd2
Levi Butler <levi.butler@redhare.software>
2021-03-03 00:56:50 +0000

    VIH-7337 health check endpoint (#64)
    Co-authored-by: Levi <levi.butler@hmcts.net>

---
+++ BookingQueueSubscriber/BookingQueueSubscriber/Contract/Responses/HealthCheckResponse.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+
+namespace BookingQueueSubscriber.Contract.Responses
+{
+    public class HealthCheckResponse
+    {
+        public HealthCheckResponse()
+        {
+            VideoApiHealth = new HealthCheck();
+            AppVersion = new ApplicationVersion();
+        }
+
+        public HealthCheck VideoApiHealth { get; set; }
+        public ApplicationVersion AppVersion { get; set; }
+
+    }
+
+    public class HealthCheck
+    {
+        public bool Successful { get; set; }
+        public string ErrorMessage { get; set; }
+        public IDictionary Data { get; set; }
+    }
+
+    public class ApplicationVersion
+    {
+        public string FileVersion { get; set; }
+        public string InformationVersion { get; set; }
+    }
+}
