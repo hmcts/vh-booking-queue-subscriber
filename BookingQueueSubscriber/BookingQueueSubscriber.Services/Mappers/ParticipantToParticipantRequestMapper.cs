9c9e9acbcc22aa200f77b83e3c313a19ebb7647f
alberto-hortiguela <alberto.hortiguela@hmcts.net>
2021-08-17 13:58:30 +0100

    VIH-8036 Adding UserRole consts

--- BookingQueueSubscriber/BookingQueueSubscriber.Services/Mappers/ParticipantToParticipantRequestMapper.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.Services/Mappers/ParticipantToParticipantRequestMapper.cs
@@ -7,6 +7,7 @@ using VideoApi.Contract.Requests;
 using LinkedParticipantType = VideoApi.Contract.Enums.LinkedParticipantType;
 using VideoWebRequests = BookingQueueSubscriber.Services.VideoWeb;
 using VideoApiRequests = VideoApi.Contract.Requests;
+using BookingQueueSubscriber.Services.Consts;
 
 namespace BookingQueueSubscriber.Services.Mappers
 {
@@ -38,11 +39,11 @@ namespace BookingQueueSubscriber.Services.Mappers
 
         private static UserRole GetUserRole(string dtoUserRole)
         {
-            if (dtoUserRole == "Judicial Office Holder")
+            if (dtoUserRole == UserRoleName.JudicialOfficeHolder)
             {
                 return UserRole.JudicialOfficeHolder;
             }
-            else if (dtoUserRole == "Staff Member")
+            else if (dtoUserRole == UserRoleName.StaffMember)
             {
                 return UserRole.StaffMember;
             }
