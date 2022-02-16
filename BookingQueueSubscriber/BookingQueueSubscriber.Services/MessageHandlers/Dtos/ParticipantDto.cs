facc2927514fd9bb8e0e705ebb573d57c294c5f4
Khizar Khan <khizarkhan.hmcts@gmail.com>
2022-02-16 11:35:23 +0000

    VIH-8703 Reference bookings api casetype group enum

--- BookingQueueSubscriber/BookingQueueSubscriber.Services/MessageHandlers/Dtos/ParticipantDto.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.Services/MessageHandlers/Dtos/ParticipantDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookingsApi.Contract.Enums;
+using System;
 using System.Collections.Generic;
 
 namespace BookingQueueSubscriber.Services.MessageHandlers.Dtos
@@ -19,22 +20,5 @@ namespace BookingQueueSubscriber.Services.MessageHandlers.Dtos
         public string Representee { get; set; }
 
         public IList<LinkedParticipantDto> LinkedParticipants { get; set; }
-    }
-
-    public enum CaseRoleGroup
-    {
-        Judge = 0,
-        Claimant = 1,
-        Defendant = 2,
-        Applicant = 3,
-        Respondent = 4,
-        Observer = 5,
-        PanelMember = 6,
-        Appellant = 7,
-        State = 8,
-        None = 9,
-        HomeOffice = 10,
-        Winger = 11,
-        StaffMember = 12
     }
 }
