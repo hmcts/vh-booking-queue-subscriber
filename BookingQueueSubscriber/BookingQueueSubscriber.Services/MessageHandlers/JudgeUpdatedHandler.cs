2cc53381d784baf924f394755e97e9a690dc8a9c
Will Craig <william.craig2@justice.gov.uk>
2022-08-22 18:22:03 +0100

    Added JudgeUpdatedHandler.cs and JudgeUpdatedIntegrationEvent.cs

---
+++ BookingQueueSubscriber/BookingQueueSubscriber.Services/MessageHandlers/JudgeUpdatedHandler.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using BookingQueueSubscriber.Services.IntegrationEvents;
+using BookingQueueSubscriber.Services.MessageHandlers.Core;
+using Microsoft.Extensions.Logging;
+
+namespace BookingQueueSubscriber.Services.MessageHandlers
+{
+    public class JudgeUpdatedHandler : IMessageHandler<JudgeUpdatedIntegrationEvent>
+    {
+        private readonly IUserCreationAndNotification _userCreationAndNotification;
+
+        public JudgeUpdatedHandler(  IUserCreationAndNotification userCreationAndNotification, ILogger<JudgeUpdatedHandler> logger)
+        {
+            _userCreationAndNotification = userCreationAndNotification;
+        }
+
+        public async Task HandleAsync(JudgeUpdatedIntegrationEvent eventMessage) 
+            => await _userCreationAndNotification.SendHearingNotificationAsync(eventMessage.Hearing, new[] {eventMessage.Judge});
+        
+
+        async Task IMessageHandler.HandleAsync(object integrationEvent) => await HandleAsync((JudgeUpdatedIntegrationEvent)integrationEvent);
+        
+    }
+}
