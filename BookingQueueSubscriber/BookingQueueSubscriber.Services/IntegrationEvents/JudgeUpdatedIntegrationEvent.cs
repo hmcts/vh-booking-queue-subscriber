0b5d01f078e1730b813e3442e800c6e6853831b8
Will Craig <william.craig2@justice.gov.uk>
2022-08-23 10:28:01 +0100

    Unused Usings removed

--- BookingQueueSubscriber/BookingQueueSubscriber.Services/IntegrationEvents/JudgeUpdatedIntegrationEvent.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.Services/IntegrationEvents/JudgeUpdatedIntegrationEvent.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using BookingQueueSubscriber.Services.MessageHandlers.Dtos;
+﻿using BookingQueueSubscriber.Services.MessageHandlers.Dtos;
 
 namespace BookingQueueSubscriber.Services.IntegrationEvents
 {
