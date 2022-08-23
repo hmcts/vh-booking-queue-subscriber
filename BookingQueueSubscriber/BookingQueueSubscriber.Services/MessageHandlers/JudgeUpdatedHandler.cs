d8224796d8657304734d6050f87a2cf178c5f20f
Will Craig <william.craig2@justice.gov.uk>
2022-08-23 14:58:42 +0100

    Update participant details

--- BookingQueueSubscriber/BookingQueueSubscriber.Services/MessageHandlers/JudgeUpdatedHandler.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.Services/MessageHandlers/JudgeUpdatedHandler.cs
@@ -1,20 +1,39 @@
+using System.Linq;
 using System.Threading.Tasks;
 using BookingQueueSubscriber.Services.IntegrationEvents;
+using BookingQueueSubscriber.Services.Mappers;
 using BookingQueueSubscriber.Services.MessageHandlers.Core;
+using BookingQueueSubscriber.Services.VideoApi;
+using Microsoft.Extensions.Logging;
 
 namespace BookingQueueSubscriber.Services.MessageHandlers
 {
     public class JudgeUpdatedHandler : IMessageHandler<JudgeUpdatedIntegrationEvent>
     {
+        private readonly IVideoApiService _apiService;
         private readonly IUserCreationAndNotification _userCreationAndNotification;
+        private ILogger _logger;
 
-        public JudgeUpdatedHandler(  IUserCreationAndNotification userCreationAndNotification)
+        public JudgeUpdatedHandler(IVideoApiService apiService, IUserCreationAndNotification userCreationAndNotification, ILogger logger)
         {
+            _logger = logger;
+            _apiService = apiService;
             _userCreationAndNotification = userCreationAndNotification;
         }
 
-        public async Task HandleAsync(JudgeUpdatedIntegrationEvent eventMessage) 
-            => await _userCreationAndNotification.SendHearingNotificationAsync(eventMessage.Hearing, new[] {eventMessage.Judge});
+        public async Task HandleAsync(JudgeUpdatedIntegrationEvent eventMessage)
+        {
+            var conferenceResponse = await _apiService.GetConferenceByHearingRefId(eventMessage.Hearing.HearingId, true);
+            var judgeResponse = conferenceResponse.Participants.SingleOrDefault(x => x.RefId == eventMessage.Judge.ParticipantId);
+            
+            if (judgeResponse != null)
+            {
+                _logger.LogError("Unable to find judge participant by ref id {ParticipantRefId} in {ConferenceId}", eventMessage.Judge.ParticipantId, conferenceResponse.Id);
+                var request = ParticipantToUpdateParticipantMapper.MapToParticipantRequest(eventMessage.Judge);
+                await _apiService.UpdateParticipantDetails(conferenceResponse.Id, judgeResponse.Id, request);
+            }
+            await _userCreationAndNotification.SendHearingNotificationAsync(eventMessage.Hearing, new[] {eventMessage.Judge});
+        } 
         
 
         async Task IMessageHandler.HandleAsync(object integrationEvent) => await HandleAsync((JudgeUpdatedIntegrationEvent)integrationEvent);
--- BookingQueueSubscriber/BookingQueueSubscriber.UnitTests/MessageHandlers/JudgeUpdatedHandlerTests.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.UnitTests/MessageHandlers/JudgeUpdatedHandlerTests.cs
@@ -1,21 +1,26 @@
+using System;
 using System.Threading.Tasks;
 using BookingQueueSubscriber.Services.IntegrationEvents;
 using BookingQueueSubscriber.Services.MessageHandlers;
 using BookingQueueSubscriber.Services.MessageHandlers.Dtos;
+using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
+using VideoApi.Contract.Requests;
 
 namespace BookingQueueSubscriber.UnitTests.MessageHandlers
 {
     public class JudgeUpdatedHandlerTests : MessageHandlerTestBase
     {
+        private readonly Mock<ILogger<JudgeUpdatedHandler>> logger = new Mock<ILogger<JudgeUpdatedHandler>>();
         [Test]
-        public async Task should_send_notification()
+        public async Task should_send_notification_and_update_participant_details()
         {
-            var messageHandler = new JudgeUpdatedHandler(UserCreationAndNotificationMock.Object);
+            var messageHandler = new JudgeUpdatedHandler(VideoApiServiceMock.Object, UserCreationAndNotificationMock.Object, logger.Object);
 
             var integrationEvent = new JudgeUpdatedIntegrationEvent() { Hearing = new HearingDto(), Judge = new ParticipantDto()};
             await messageHandler.HandleAsync(integrationEvent);
+            VideoApiServiceMock.Verify(x => x.UpdateParticipantDetails(It.IsAny<Guid>(), It.IsAny<Guid>(), It.IsAny<UpdateParticipantRequest>()), Times.Once);
             UserCreationAndNotificationMock.Verify(x => x.SendHearingNotificationAsync(It.IsAny<HearingDto>(), It.IsAny<ParticipantDto[]>()), Times.Once);
         }
     }
