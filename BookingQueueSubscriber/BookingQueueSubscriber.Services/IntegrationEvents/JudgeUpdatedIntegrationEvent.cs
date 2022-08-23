4113fa79a59fcd47503cd4be7c576a85a7db34ec
William Craig <38870219+will-craig@users.noreply.github.com>
2022-08-23 17:19:21 +0100

    VIH-9230 notify when judge contact email sent (#101)
    * Added JudgeUpdatedHandler.cs and JudgeUpdatedIntegrationEvent.cs
    
    * Unit Test
    
    * Unused Usings removed
    
    * Update participant details
    
    * Updated tests

---
+++ BookingQueueSubscriber/BookingQueueSubscriber.Services/IntegrationEvents/JudgeUpdatedIntegrationEvent.cs
@@ -0,0 +1,10 @@
+﻿using BookingQueueSubscriber.Services.MessageHandlers.Dtos;
+
+namespace BookingQueueSubscriber.Services.IntegrationEvents
+{
+    public class JudgeUpdatedIntegrationEvent : IIntegrationEvent
+    {
+        public HearingDto Hearing { get; set; }
+        public ParticipantDto Judge { get; set; }
+    }
+}
