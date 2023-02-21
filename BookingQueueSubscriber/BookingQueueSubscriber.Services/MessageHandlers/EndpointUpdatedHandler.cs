e064e4165c1e62a2754752fffc96482fc15ddd65
Robert Twyford <robert.twyford@version1.com>
2023-02-21 09:55:54 +0000

    1. Added for loop with a retry limit to get the conference details which includes the list of conference participants 2. Attempting to pull defence advocate from the list of participants with matching contact email to the defence advocate sent in event message 3. Added logging if retry limit is reached before defence advocate email is found 4. Added thread sleeping between each retry. Determined by the RetrySleep value 5. Added logging if conference pulled down for this is null

--- BookingQueueSubscriber/BookingQueueSubscriber.Services/MessageHandlers/EndpointUpdatedHandler.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.Services/MessageHandlers/EndpointUpdatedHandler.cs
@@ -1,8 +1,10 @@
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using BookingQueueSubscriber.Services.IntegrationEvents;
 using BookingQueueSubscriber.Services.MessageHandlers.Core;
 using BookingQueueSubscriber.Services.VideoApi;
+using Microsoft.Extensions.Logging;
 using VideoApi.Contract.Requests;
 using VideoApi.Contract.Responses;
 
@@ -11,26 +13,57 @@ namespace BookingQueueSubscriber.Services.MessageHandlers
     public class EndpointUpdatedHandler : IMessageHandler<EndpointUpdatedIntegrationEvent>
     {
         private readonly IVideoApiService _videoApiService;
+        private readonly ILogger<EndpointUpdatedHandler> _logger;
+        private const int RetryLimit = 3;
+        private const int RetrySleep = 3000;
 
-        public EndpointUpdatedHandler(IVideoApiService videoApiService)
+        public EndpointUpdatedHandler(IVideoApiService videoApiService, ILogger<EndpointUpdatedHandler> logger)
         {
             _videoApiService = videoApiService;
+            _logger = logger;
         }
 
         public async Task HandleAsync(EndpointUpdatedIntegrationEvent eventMessage)
         {
-            var conference = await _videoApiService.GetConferenceByHearingRefId(eventMessage.HearingId);
             ParticipantDetailsResponse defenceAdvocate = null;
+            ConferenceDetailsResponse conference = null;
+
             if (!string.IsNullOrEmpty(eventMessage.DefenceAdvocate))
             {
-                defenceAdvocate = conference.Participants.Single(x => x.ContactEmail ==
-                    eventMessage.DefenceAdvocate);
+                for (var retry = 0; retry <= RetryLimit; retry++)
+                {
+                    conference = await _videoApiService.GetConferenceByHearingRefId(eventMessage.HearingId);
+
+                    defenceAdvocate = conference.Participants.SingleOrDefault(x => x.ContactEmail ==
+                                eventMessage.DefenceAdvocate);
+
+                    if (defenceAdvocate != null)
+                    {
+                        break;
+                    }
+
+                    if (retry == RetryLimit)
+                    {
+                        _logger.LogError("Unable to find defence advocate email by hearing id {HearingId}", eventMessage.HearingId);
+                        break;
+                    }
+
+                    Thread.Sleep(RetrySleep);
+                }
             }
-            await _videoApiService.UpdateEndpointInConference(conference.Id, eventMessage.Sip, new UpdateEndpointRequest
+
+            if (conference != null)
             {
-                DisplayName = eventMessage.DisplayName,
-                DefenceAdvocate = defenceAdvocate?.Username
-            });
+                await _videoApiService.UpdateEndpointInConference(conference.Id, eventMessage.Sip, new UpdateEndpointRequest
+                {
+                    DisplayName = eventMessage.DisplayName,
+                    DefenceAdvocate = defenceAdvocate?.Username
+                });
+            }
+            else
+            {
+                _logger.LogError("Unable to find conference by hearing id {HearingId}", eventMessage.HearingId);
+            }
         }
 
         async Task IMessageHandler.HandleAsync(object integrationEvent)
