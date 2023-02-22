3d27d19d8b8600700583e6d21bc276c5e81647e5
twyfordr273 <123372987+twyfordr273@users.noreply.github.com>
2023-02-22 15:09:28 +0000

    VIH-9527 - Video Web Last minute booking representatives unable to call endpoint for private consultation (#166)
    * 1. Added for loop with a retry limit to get the conference details which includes the list of conference participants
    2. Attempting to pull defence advocate from the list of participants with matching contact email to the defence advocate sent in event message
    3. Added logging if retry limit is reached before defence advocate email is found
    4. Added thread sleeping between each retry. Determined by the RetrySleep value
    5. Added logging if conference pulled down for this is null
    
    * 1. Updated Unit Tests to include ILogger
    2. Add check for conference prior to getting participants from conference to get defence advocate
    
    * 1. Prevent Update of Endpoint if Conference returns null
    2. Added Unit Tests
    
    * 1. Will now break out of the for loop if conference is null. The conference will need to be gotten multiple times to get the updated participants list, but it should always have a value for the conference
    2. Moved the for loop with the retries into its own method to get rid of the code smell
    2. Updated tests to reflect the refactored code
    
    * 1. removed the 'this' from this.GetDefenceAdvocate(conference, eventMessage);
    
    * Changed object type of conference from ConferenceDetailsResponse to var

--- BookingQueueSubscriber/BookingQueueSubscriber.Services/MessageHandlers/EndpointUpdatedHandler.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.Services/MessageHandlers/EndpointUpdatedHandler.cs
@@ -1,8 +1,10 @@
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using BookingQueueSubscriber.Services.IntegrationEvents;
 using BookingQueueSubscriber.Services.MessageHandlers.Core;
 using BookingQueueSubscriber.Services.VideoApi;
+using Microsoft.Extensions.Logging;
 using VideoApi.Contract.Requests;
 using VideoApi.Contract.Responses;
 
@@ -11,26 +13,69 @@ namespace BookingQueueSubscriber.Services.MessageHandlers
     public class EndpointUpdatedHandler : IMessageHandler<EndpointUpdatedIntegrationEvent>
     {
         private readonly IVideoApiService _videoApiService;
+        private readonly ILogger<EndpointUpdatedHandler> _logger;
+        private const int RetryLimit = 3;
+        private const int RetrySleep = 3000;
 
-        public EndpointUpdatedHandler(IVideoApiService videoApiService)
+        public EndpointUpdatedHandler(IVideoApiService videoApiService, ILogger<EndpointUpdatedHandler> logger)
         {
             _videoApiService = videoApiService;
+            _logger = logger;
         }
 
         public async Task HandleAsync(EndpointUpdatedIntegrationEvent eventMessage)
         {
             var conference = await _videoApiService.GetConferenceByHearingRefId(eventMessage.HearingId);
             ParticipantDetailsResponse defenceAdvocate = null;
+            
             if (!string.IsNullOrEmpty(eventMessage.DefenceAdvocate))
             {
-                defenceAdvocate = conference.Participants.Single(x => x.ContactEmail ==
-                    eventMessage.DefenceAdvocate);
+                defenceAdvocate = await GetDefenceAdvocate(conference, eventMessage);
             }
-            await _videoApiService.UpdateEndpointInConference(conference.Id, eventMessage.Sip, new UpdateEndpointRequest
+
+            if (conference != null)
             {
-                DisplayName = eventMessage.DisplayName,
-                DefenceAdvocate = defenceAdvocate?.Username
-            });
+                await _videoApiService.UpdateEndpointInConference(conference.Id, eventMessage.Sip, new UpdateEndpointRequest
+                {
+                    DisplayName = eventMessage.DisplayName,
+                    DefenceAdvocate = defenceAdvocate?.Username
+                });
+            }
+        }
+
+        private async Task<ParticipantDetailsResponse> GetDefenceAdvocate(ConferenceDetailsResponse conference, EndpointUpdatedIntegrationEvent eventMessage)
+        {
+            ParticipantDetailsResponse defenceAdvocate = null;
+
+            for (var retry = 0; retry <= RetryLimit; retry++)
+            {
+                if (conference == null)
+                {
+                    _logger.LogError("Unable to find conference by hearing id {HearingId}", eventMessage.HearingId);
+                    break;
+                }
+                else
+                {
+                    defenceAdvocate = conference.Participants.SingleOrDefault(x => x.ContactEmail ==
+                            eventMessage.DefenceAdvocate);
+
+                    if (defenceAdvocate != null)
+                    {
+                        break;
+                    }
+
+                    if (retry == RetryLimit)
+                    {
+                        _logger.LogError("Unable to find defence advocate email by hearing id {HearingId}", eventMessage.HearingId);
+                        break;
+                    }
+                }
+
+                Thread.Sleep(RetrySleep);
+                conference = await _videoApiService.GetConferenceByHearingRefId(eventMessage.HearingId);
+            }
+
+            return defenceAdvocate;
         }
 
         async Task IMessageHandler.HandleAsync(object integrationEvent)
