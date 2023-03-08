4808eb85fa7d1a3904a8a89b69486a369dc38300
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-03-08 22:24:45 +0000

    remove AC tests

--- BookingQueueSubscriber/BookingQueueSubscriber.AcceptanceTests/Configuration/Data/EmailData.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BookingQueueSubscriber.AcceptanceTests.Configuration.Data
-{
-    public static class EmailData
-    {
-        public const string NON_EXISTENT_USERNAME = "made_up_username@hmcts.net";
-    }
-}
