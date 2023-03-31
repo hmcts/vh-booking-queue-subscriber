616b9a29cea213dc5791fe36569e476e810f9612
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-03-31 15:21:14 +0100

    remove dupe test

--- BookingQueueSubscriber/BookingQueueSubscriber.UnitTests/VideoApiServiceTests/GetEndpointsForConferenceTests.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.UnitTests/VideoApiServiceTests/GetEndpointsForConferenceTests.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using BookingQueueSubscriber.Services.VideoApi;
 using FizzWare.NBuilder;
 using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using NUnit.Framework;
+using RichardSzalay.MockHttp;
 using VideoApi.Client;
 using VideoApi.Contract.Responses;
 
@@ -15,22 +19,35 @@ namespace BookingQueueSubscriber.UnitTests.VideoApiServiceTests
     public class GetEndpointsForConferenceTests
     {
         [Test]
-        public async Task should_get_endpoints_for_a_conference()
+        public async Task should_get_endpoints_for_conference_using_mock_http()
         {
             // arrange
+            var baseAddress = "http://video-api";
             var conferenceId = Guid.NewGuid();
             var endpoints = Builder<EndpointResponse>.CreateListOfSize(3).Build().ToList();
-            
-            var videoApiClientMock = new Mock<IVideoApiClient>();
-            var loggerMock = new Mock<ILogger<VideoApiService>>();
-            var videoApiService = new VideoApiService(videoApiClientMock.Object, loggerMock.Object);
-            
-            videoApiClientMock.Setup(x => x.GetEndpointsForConferenceAsync(conferenceId)).ReturnsAsync(endpoints);
-            
+            var json = JsonConvert.SerializeObject(endpoints, new JsonSerializerSettings
+            {
+                ContractResolver = new DefaultContractResolver
+                {
+                    NamingStrategy = new SnakeCaseNamingStrategy()
+                },
+                Formatting = Formatting.Indented
+            });
+
+            var mockHttp = new MockHttpMessageHandler();
+            var request = mockHttp.When($"{baseAddress}/conferences/{conferenceId}/endpoints")
+                .Respond(HttpStatusCode.OK, "application/json", json);
+            var httpClient = mockHttp.ToHttpClient();
+            httpClient.BaseAddress = new Uri(baseAddress);
+
+            var videoApiClient = VideoApiClient.GetClient(baseAddress, httpClient);
+            var videoApiService = new VideoApiService(videoApiClient, new Mock<ILogger<VideoApiService>>().Object);
+
             // act
-            var result =await  videoApiService.GetEndpointsForConference(conferenceId);
-            
+            var result = await videoApiService.GetEndpointsForConference(conferenceId);
+
             // assert
+            mockHttp.GetMatchCount(request).Should().Be(1);
             result.Should().NotBeNullOrEmpty();
             result.Should().BeEquivalentTo(endpoints);
         }
