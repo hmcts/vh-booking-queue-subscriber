3fc4915ed1f186af006046db6fc76a328d77403d
Oliver Scott <oliver.scott@justice.gov.uk>
2023-04-21 16:15:55 +0100

    Execute tasks in parallel

--- BookingQueueSubscriber/BookingQueueSubscriber.Services/UserApi/User.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.Services/UserApi/User.cs
@@ -5,5 +5,6 @@
         public string UserId { get; set; }
         public string Password { get; set; }
         public string UserName { get; set; }
+        public string ContactEmail { get; set; }
     }
 }
