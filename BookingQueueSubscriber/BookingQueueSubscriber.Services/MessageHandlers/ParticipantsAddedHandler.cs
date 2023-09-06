7da261f883e7062fed4b41d446eb72f3430ee7b0
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-09-06 18:40:17 +0100

    Add logging to ParticipantUpdatedHandler.cs and VideoWebService.cs

--- BookingQueueSubscriber/BookingQueueSubscriber.Services/MessageHandlers/ParticipantsAddedHandler.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.Services/MessageHandlers/ParticipantsAddedHandler.cs
@@ -10,13 +10,15 @@ namespace BookingQueueSubscriber.Services.MessageHandlers
         private readonly IVideoApiService _videoApiService;
         private readonly IVideoWebService _videoWebService;
         private readonly IUserCreationAndNotification _userCreationAndNotification;
+        private readonly ILogger<ParticipantsAddedHandler> _logger;
 
         public ParticipantsAddedHandler(IVideoApiService videoApiService, IVideoWebService videoWebService, 
-            IUserCreationAndNotification userCreationAndNotification)
+            IUserCreationAndNotification userCreationAndNotification, ILogger<ParticipantsAddedHandler> logger)
         {
             _videoApiService = videoApiService;
             _videoWebService = videoWebService;
             _userCreationAndNotification = userCreationAndNotification;
+            _logger = logger;
         }
 
         public async Task HandleAsync(ParticipantsAddedIntegrationEvent eventMessage)
@@ -26,6 +28,7 @@ namespace BookingQueueSubscriber.Services.MessageHandlers
             await _userCreationAndNotification.SendHearingNotificationAsync(eventMessage.Hearing, eventMessage.Participants);
 
             var conference = await _videoApiService.GetConferenceByHearingRefId(eventMessage.Hearing.HearingId);
+            _logger.LogInformation("Update participant list for Conference {ConferenceId}", conference.Id);
             var request = new AddParticipantsToConferenceRequest
             {
                 Participants = eventMessage.Participants
