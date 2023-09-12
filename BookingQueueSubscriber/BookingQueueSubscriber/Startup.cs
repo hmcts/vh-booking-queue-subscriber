2ac5dc6ede9f193e96025c33c53865d29f895232
Shaed Parkar <shaed.parkar@slimsolutions.company>
2023-09-12 09:30:32 +0100

    register AppInsights using extension from lib

--- BookingQueueSubscriber/BookingQueueSubscriber/Startup.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber/Startup.cs
@@ -85,7 +85,7 @@ namespace BookingQueueSubscriber
             services.AddTransient<IUserCreationAndNotification, UserCreationAndNotification>();
             services.AddTransient<NotificationServiceTokenHandler>();
             services.AddTransient<UserServiceTokenHandler>();
-            services.AddLogging(builder => builder.AddApplicationInsights());
+            services.AddApplicationInsightsTelemetry();
             
             RegisterMessageHandlers(services);
 
