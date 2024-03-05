245372ab4322c163fdeeb9f98e61084374aa6c52
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-03-05 11:36:40 +0000

    removed handler and added sendNotification property

--- BookingQueueSubscriber/BookingQueueSubscriber.Services/IntegrationEvents/JudgeUpdatedNoNotificationIntegrationEvent.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BookingQueueSubscriber.Services.IntegrationEvents
-{
-    public class JudgeUpdatedNoNotificationIntegrationEvent : IIntegrationEvent
-    {
-        public HearingDto Hearing { get; set; }
-        public ParticipantDto Judge { get; set; }
-    }
-}
