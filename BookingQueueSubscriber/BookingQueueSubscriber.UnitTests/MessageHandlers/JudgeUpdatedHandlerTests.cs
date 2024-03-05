d50ca241b43c8695c49334bb2972a5869552097a
marcogagliardi <marco.gagliardi@justice.gov.uk>
2024-03-05 11:54:27 +0000

    Update JudgeUpdatedHandlerTests.cs

--- BookingQueueSubscriber/BookingQueueSubscriber.UnitTests/MessageHandlers/JudgeUpdatedHandlerTests.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.UnitTests/MessageHandlers/JudgeUpdatedHandlerTests.cs
@@ -46,6 +46,28 @@ namespace BookingQueueSubscriber.UnitTests.MessageHandlers
             VideoApiServiceMock.Verify(x => x.UpdateParticipantDetails(It.IsAny<Guid>(), It.IsAny<Guid>(), It.IsAny<UpdateParticipantRequest>()), Times.Once);
             UserCreationAndNotificationMock.Verify(x => x.SendHearingNotificationAsync(It.IsAny<HearingDto>(), It.IsAny<ParticipantDto[]>()), Times.Once);
         }
+        
+        [Test]
+        public async Task should_not_send_notification_and_update_participant_details()
+        {
+            var messageHandler = new JudgeUpdatedHandler(VideoApiServiceMock.Object, UserCreationAndNotificationMock.Object, _logger.Object);
+
+            var integrationEvent = new JudgeUpdatedIntegrationEvent()
+            {
+                Hearing = new HearingDto(), 
+                Judge = new ParticipantDto
+                {
+                    ContactEmail = "new_judge@email.com", 
+                    UserRole = "Judge", 
+                    HearingRole = "Judge"
+                },
+                SendNotification = false
+            };
+            await messageHandler.HandleAsync(integrationEvent);
+            VideoApiServiceMock.Verify(x => x.UpdateParticipantDetails(It.IsAny<Guid>(), It.IsAny<Guid>(), It.IsAny<UpdateParticipantRequest>()), Times.Once);
+            UserCreationAndNotificationMock.Verify(x => x.SendHearingNotificationAsync(It.IsAny<HearingDto>(), It.IsAny<ParticipantDto[]>()), Times.Never);
+        }
+
         [Test]
         public async Task should_not_send_notification_but_still_update_participant_details()
         {
