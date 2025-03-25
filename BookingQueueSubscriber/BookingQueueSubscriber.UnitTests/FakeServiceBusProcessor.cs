5c9791b5a17322cbebe8b0b1d2f9c114e116086c
Oliver Scott <oliver.scott@justice.gov.uk>
2025-03-25 18:40:19 +0000

    Use #pragma

--- BookingQueueSubscriber/BookingQueueSubscriber.UnitTests/FakeServiceBusProcessor.cs
+++ BookingQueueSubscriber/BookingQueueSubscriber.UnitTests/FakeServiceBusProcessor.cs
@@ -8,8 +8,10 @@ namespace BookingQueueSubscriber.UnitTests;
 [SuppressMessage("Usage", "CS0067", Justification = "Events are used only by unit tests")]
 public class FakeServiceBusProcessor : IServiceBusProcessorWrapper
 {
+    #pragma warning disable CS0067
     public event Func<ProcessMessageEventArgs, Task> ProcessMessageAsync;
     public event Func<ProcessErrorEventArgs, Task> ProcessErrorAsync;
+    #pragma warning disable CS0067
     public bool IsRunning { get; private set; }
 
     public Task StartProcessingAsync(CancellationToken cancellationToken)
